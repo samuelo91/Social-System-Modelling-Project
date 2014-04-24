14d0a128c8bd159cb6e0ade3afdcf5e4cf83ef83
Samuel <samuelo@student.ethz.ch>
2014-04-24 23:34:47 +0200

    comments added

--- code/testModel.m
+++ code/testModel.m
@@ -2,14 +2,16 @@ function [x,y] = testModel(steps)
 
 %Global variables
 NOAGENTS = 30;
+START_DISTANCE = 0.2
 dt = 0.1
 
 %agent x, y, vx, vy, desVel, ex, ey, vavg
 agents = zeros(NOAGENTS,8);
 agentsUpdated = zeros(NOAGENTS,8);
 
+%Initalize agents at the left side with y distance START_DISTANCE apart
 for a = 1:NOAGENTS
-    agent = [0,a*0.2,0,0,1.3,1,0,0];
+    agent = [0,a*START_DISTANCE,0,0,1.3,1,0,0];
     agents(a,:) = agent;
 end
 
@@ -27,7 +29,7 @@ for time = 1:dt:steps
         for i = 1:NOAGENTS
             
             if (a~=i)
-                %Do calc for all other agents
+                %Do calc for all other agents except oneself
                 otheragent = agents(i,:);
                 [pedFx,pedFy] = pedestrianF(agent(1),agent(2),otheragent(1),otheragent(2));
                 agent(3) = agent(3) + dt*pedFx;
@@ -42,14 +44,19 @@ for time = 1:dt:steps
         agent(2) = agent(2) + dt*agent(4);
         agent(8) = averageSpeed(agent(8),sqrt(agent(3)^2 + agent(4)^2), time);
         
+        %We use a new matrix agentsUpdated so all agents are updated in one
+        %step and for updates depending on the others positions the old
+        %positions of already updated agents are used
         agentsUpdated(a,:) = agent;
         
     end
     
     agents = agentsUpdated;
+    
     plot(agents(:,1),agents(:,2),'Marker', 'o','LineStyle', 'none')
     set (gca, 'YLimMode', 'Manual', 'YLim', [-5 35], 'XLim', [0 30]);
     drawnow
+    %0.1 seconds pause so the agents move in 'realtime'
     pause(0.1);
 end
 
