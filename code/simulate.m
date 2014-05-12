346a87fc27dadf39e7975e4ebbfc029b724f170d
Samuel <samuelo@student.ethz.ch>
2014-05-12 20:05:15 +0200

    commented code

--- code/simulate.m
+++ code/simulate.m
@@ -11,19 +11,23 @@ vidObj.FrameRate = 20;
 open(vidObj);
 
 for time = 1:framesNo
+    %plot with people per square or without
     if(mode==1)
         hold on
-        imagesc(0:15,0:15,pplSqData(:,:,time),[0,8]); 
+        %plot people per square
+        imagesc(0:15,0:15,pplSqData(:,:,time),[0,8]);
         colormap jet;
-        colorbar
+        cb = colorbar('vert');
+        zlab = get(cb,'ylabel');
+        set(zlab,'String','people per square meter'); 
     end
+    %plot agents in black
     plot(positionDataX(:,time),positionDataY(:,time), 'Marker', 'o','LineStyle', 'none','MarkerSize', 10, 'MarkerEdgeColor','k')
     set (gca, 'YLimMode', 'Manual', 'YLim', [0 15], 'XLim', [0 15]);
     [m,n] = size(walls);
     for w = 1 : m
         line([walls(w,1);walls(w,3)],[walls(w,2);walls(w,4)],'Color','k','LineWidth',2)
     end
-    drawnow
     if(mode==1)
         hold off
     end
--- code/testModel.m
+++ code/testModel.m
@@ -3,93 +3,102 @@ function testModel(steps,filename, situation)
 %situation is either 0 --> cross situation
 %                    1 --> curve situation
 
+%save the seed on the Random Stream Generator so the results can be
+%reproduced
+stream = RandStream.getGlobalStream;
+
 %Global variables
-NOAGENTS = 150;
+NOAGENTS = 5;
 SPEED_MEAN = 1.3;
 SPEED_DISTR = 0.1;
 dt = 0.05;
 framesNo = (steps-1)/dt+1;
 
-%agent = [x, y, vx, vy, desVel, type, unused, vavg, waypointNO]
+%agent = [x-position, y-position, x-speed, y-speed, desired Velocity, which start side, waypointNO, average speed]
 %agents = [agent1; agent2; agent3;...]
-agents = zeros(NOAGENTS,9);
-agentsUpdated = zeros(NOAGENTS,9);
+agents = zeros(NOAGENTS,8);
+agentsUpdated = zeros(NOAGENTS,8);
 positionDataX = zeros(NOAGENTS,framesNo);
 positionDataY = zeros(NOAGENTS,framesNo);
 pplSqData = zeros(15,15,framesNo);
 
 if(situation == 0)
-    %Init destination zone
+    %Init waypoints to reach destination
     waypointsA = [10,6,10,9;
                   15,5,15,10;
-                  25,5,25,10];
+                  16,5,16,10];
     waypointsB = [6,5,9,5;
                   5,0,10,0;
-                  5,-10,10,-10];
+                  5,-1,10,-1];
 
     %walls form a cross
-    walls = [-10,5,5,5;
-             -10,10,5,10;
-             10,5,15,5;
-             10,10,15,10;
-             5,0,5,5;
-             5,10,5,25;
-             10,0,10,5;
-             10,10,10,25];
+    walls = [-20,5,5,5;
+             -20,10,5,10;
+             10,5,35,5;
+             10,10,35,10;
+             5,-10,5,5;
+             5,10,5,35;
+             10,-10,10,5;
+             10,10,10,35];
          
+    %Obstacles are added here:
     %walls = [walls; makeObstacleRect(7,7,8,8)];
     %walls = [walls; makeObstacleTriangle(7,7,8,8,6,9)];      
 end
 
 if(situation == 1)
-    %Init destination zone
-    waypointsA = [0,7,7,7;
-                  7,7,2,14;
-                  7,7,7,14;
+    %Init waypoints to reach destination
+    waypointsA = [2,7,7,7;
+                  3,13,8,8;
                   15,7,15,15;
-                  25,7,25,15];
-    waypointsB = [7,7,7,14;
-                  7,7,2,14;
-                  0,7,7,7;
+                  16,7,16,15];
+    waypointsB = [7,7,7,13;
+                  1,12,6,6;
                   0,0,7,0;
-                  0,-10,7,-10];
+                  0,-1,7,-1];
 
     %walls form a curve
     walls = [0,-10,0,15;
              0,15,25,15;
              7,-10,7,7;
              7,7,25,7];
+         
+    %Obstacles are added here: 
+    %walls = [walls;5,7,7,9;4,8,6,10;3,9,5,11];
 end
 
-%Initalize agents at the left side with y distance START_DISTANCE apart
+%Initalize agents according to situation
 for a = 1:NOAGENTS
+    
     % Sets the desired speed of the agent in a normaldistribution with
     % SPEED_MEAN +- SPEED_DISRTR
     speed = SPEED_MEAN + sqrt(SPEED_DISTR)*randn;
     
     if(situation == 0)
-        % random number between a and b: a+(b-a)*rand
-        if(a<NOAGENTS/2)
-            posx = -5 + (2-(-2))*rand;
+        %agents on left side
+        if(a<=NOAGENTS/2)
+            posx = -5 + 4*rand;
             posy = 5 + (10-5)*rand;
-            agent = [posx,posy,0,0,speed,0,0,speed,1];
+            agent = [posx,posy,speed,0,speed,0,1,speed];
+        %agents on top    
         else
             posx = 5 + (10-5)*rand;
-            posy = -5 + (2-(-2))*rand;
-            agent = [posx,15-posy,0,0,speed,1,-1,speed,1];
+            posy = -5 + 4*rand;
+            agent = [posx,15-posy,0,-speed,speed,1,1,speed];
         end
     end
     
     if(situation == 1)
-        % random number between a and b: a+(b-a)*rand
-        if(a<NOAGENTS/2)
+        %agents at the bottom
+        if(a<=NOAGENTS/2)
             posx = 0 + (7-(0))*rand;
             posy = -5 + (0-(-5))*rand;
-            agent = [posx,posy,0,0,speed,0,0,speed,1];
+            agent = [posx,posy,0,speed,speed,0,1,speed];
+        %agents at the right side    
         else
             posx = 15 + (20-15)*rand;
             posy = 0 + (7-(0))*rand;
-            agent = [posx,15-posy,0,0,speed,1,-1,speed,1];
+            agent = [posx,15-posy,-speed,0,speed,1,1,speed];
         end
     end
     
@@ -97,37 +106,72 @@ for a = 1:NOAGENTS
 end
 
 counter=1;
+%----------------START OF SIMULATION---------------------------------------
 % time loop
 for time = 1:dt:steps
     pplSqUnit = zeros(15,15);
+    %-----------FOR EACH AGENT DO CALCULATIONS-----------------------------
     % agent loop
     for a = 1:NOAGENTS   
         agent = agents(a,:);
         
-        %Set up desired destination for each agent
+        %------Set up desired destination for the agent-------------------
         if(agent(6)==0)
-            [ex,ey,d] = vectorFromWall(waypointsA(agent(9),:),agent(1),agent(2));
+            [ex,ey,d] = vectorFromWall(waypointsA(agent(7),:),agent(1),agent(2));
         else
-            [ex,ey,d] = vectorFromWall(waypointsB(agent(9),:),agent(1),agent(2));
+            [ex,ey,d] = vectorFromWall(waypointsB(agent(7),:),agent(1),agent(2));
         end
-        if(d<1)
+        % waypoint is reached
+        if(d<0.5)
             [s,dontcare] = size(waypointsA);
-            if (agent(9) < s)
-                agent(9) = agent(9)+1;
+            %agent not at en-destination yet -> set next waypoint
+            if (agent(7) < s)
+                agent(7) = agent(7)+1;
                 if(agent(6)==0)
-                    [ex,ey,d] = vectorFromWall(waypointsA(agent(9),:),agent(1),agent(2));
+                    [ex,ey,d] = vectorFromWall(waypointsA(agent(7),:),agent(1),agent(2));
                 else
-                    [ex,ey,d] = vectorFromWall(waypointsB(agent(9),:),agent(1),agent(2));
+                    [ex,ey,d] = vectorFromWall(waypointsB(agent(7),:),agent(1),agent(2));
+                end
+            %agent at end-destination -> set agent back into start area
+            else
+                speed = SPEED_MEAN + sqrt(SPEED_DISTR)*randn;
+
+                if(situation == 0)
+                    if(agent(6)==0)
+                        posx = -5 + (2-(-2))*rand;
+                        posy = 5 + (10-5)*rand;
+                        agent = [posx,posy,speed,0,speed,0,1,speed];
+                    else
+                        posx = 5 + (10-5)*rand;
+                        posy = -5 + (2-(-2))*rand;
+                        agent = [posx,15-posy,0,-speed,speed,1,1,speed];
+                    end
+                end
+
+                if(situation == 1)
+                    if(agent(6)==0)
+                        posx = 0 + (7-(0))*rand;
+                        posy = -5 + (0-(-5))*rand;
+                        agent = [posx,posy,0,speed,speed,0,1,speed];
+                    else
+                        posx = 15 + (20-15)*rand;
+                        posy = 0 + (7-(0))*rand;
+                        agent = [posx,15-posy,-speed,0,speed,1,1,speed];
+                    end
                 end
             end
         end    
         
-        %Acceleration force of every agent is calculated
+        %-------Acceleration force of every agent is calculated------------
         [accFx,accFy] = accelerationF(-ex, -ey, agent(3), agent(4), agent(8), agent(5)); 
+        %Add acceleration force to velocity
+        agent(3) = agent(3) + dt*accFx;
+        agent(4) = agent(4) + dt*accFy;
         
-        %Wall forces for every agent
+        %-------Wall forces for every agent--------------------------------
         mind = 10000;
         [noWalls,dontcare] = size(walls);
+        %calculate nearest wall
         for w = 1:noWalls
             wall = walls(w,:);
             [vx,vy,d] = vectorFromWall(wall,agent(1),agent(2)); 
@@ -136,16 +180,14 @@ for time = 1:dt:steps
                 minn = [vx,vy];
             end
         end
-        
+        %Influence of nearest wall on agent
         [wallFx, wallFy] = wallF(mind, minn);
         agent(3) = agent(3) + dt*wallFx;
         agent(4) = agent(4) + dt*wallFy; 
         
-        %Simplified Force from other agents (only considered second part of
-        %formula)
+        %------Force from other agents-------------------------------------
         
         for i = 1:NOAGENTS
-            
             if (a~=i)
                 %Do calc for all other agents except oneself
                 otheragent = agents(i,:);
@@ -156,15 +198,16 @@ for time = 1:dt:steps
             end
               
         end
-        %Add acceleration force to velocity
-        agent(3) = agent(3) + dt*accFx;
-        agent(4) = agent(4) + dt*accFy;
-        %Update position according to new velocity
+
+        %-------Update position according to new velocity------------------
         agent(1) = agent(1) + dt*agent(3);
         agent(2) = agent(2) + dt*agent(4);
+        
+        %------Save in which square meter the agent is in------------------
         if(ceil(agent(2)) >0 & ceil(agent(1)) >0 & ceil(agent(2))<=15 & ceil(agent(1))<=15);
             pplSqUnit(ceil(agent(2)),ceil(agent(1))) = pplSqUnit(ceil(agent(2)),ceil(agent(1)))+1;
         end
+        
         %Update average speed
         agent(8) = averageSpeed(agent(8),sqrt(agent(3)^2 + agent(4)^2), time);
         
@@ -176,6 +219,7 @@ for time = 1:dt:steps
     end
     
     agents = agentsUpdated;
+    %save data into matrices for plotting later
     positionDataX(:,counter) = agents(:,1);
     positionDataY(:,counter) = agents(:,2);
     pplSqData (:,:,counter) = pplSqUnit;
@@ -223,6 +267,7 @@ pedFy = 3*exp((0.6-norm(d))/0.2)* d(2)/norm(d);
 
 end
 
+%the most direct vector from a wall to a position(x,y) and its length
 function [vx,vy,d] = vectorFromWall(wall,x,y)
 
     vectorA = [wall(1),wall(2)] - [wall(3),wall(4)];
@@ -232,9 +277,11 @@ function [vx,vy,d] = vectorFromWall(wall,x,y)
     if(t<0)
         n = vectorB;
         d = norm(n);
+        n = n/norm(n);
     else if(t>1)
         n = [x,y] - [wall(1),wall(2)];
         d = norm(n);
+        n = n/norm(n);
         else
             n = [-vectorA(2),vectorA(1)];
             d = abs(det([vectorA;vectorB])) / norm(vectorA);
@@ -246,6 +293,7 @@ function [vx,vy,d] = vectorFromWall(wall,x,y)
     
 end
 
+%creates a rectangular obstacle out of walls
 function obstacle = makeObstacleRect(x,y,x2,y2)
 
 obstacle = [x,y,x,y2;
@@ -255,6 +303,7 @@ obstacle = [x,y,x,y2;
         
 end
 
+%creates a triangular obstacle out of walls
 function obstacle = makeObstacleTriangle(x,y,x2,y2,x3,y3)
 
 obstacle = [x,y,x2,y2;
