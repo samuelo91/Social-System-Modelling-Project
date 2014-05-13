772ba7a544be8daa0c997eb61af38cf74dcb8b5f
Samuel <samuelo@student.ethz.ch>
2014-05-13 19:45:54 +0200

    colored videos and png

--- code/maxPeopleOnSquare.m
+++ code/maxPeopleOnSquare.m
@@ -20,11 +20,11 @@ subplot(1,2,1)
 plot(ppsVsTime)
 axis([0 framesNo 0 8])
 tlhand = get(gca,'title');
-set(tlhand,'string',sprintf('number of people on the square meter \n with highest density at each frame'),'fontsize',16);
+set(tlhand,'string',sprintf('number of people \n on the square meter with highest density \n at each frame'),'fontsize',20);
 xlhand = get(gca,'xlabel');
-set(xlhand,'string','frame','fontsize',14);
+set(xlhand,'string','frame','fontsize',18);
 ylhand = get(gca,'ylabel');
-set(ylhand,'string','max people/m^2','fontsize',14);
+set(ylhand,'string','max people/m^2','fontsize',18);
 subplot(1,2,2)
 hold on
 imagesc(0:15,0:15,maxPerSquare,[0,8]);
@@ -34,14 +34,14 @@ for w = 1 : m
 end
 axis([0 15 0 15])
 tlhand = get(gca,'title');
-set(tlhand,'string',sprintf('maximal number of people on each \n square meter in xy-space'),'fontsize',16);
+set(tlhand,'string',sprintf('maximal number of people \n on each square meter \n in xy-space'),'fontsize',20);
 colormap jet;
 cb = colorbar('vert');
 zlab = get(cb,'ylabel');
-set(zlab,'String','people per square meter','FontSize',14);
+set(zlab,'String','people per square meter','FontSize',18);
 xlhand = get(gca,'xlabel');
-set(xlhand,'string','x-position','fontsize',14);
+set(xlhand,'string','x-position','fontsize',18);
 ylhand = get(gca,'ylabel');
-set(ylhand,'string','y-position','fontsize',14);
+set(ylhand,'string','y-position','fontsize',18);
 %print -depsc2 test.eps; 
 hold off
